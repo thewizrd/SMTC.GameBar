12b1045d1c53cff7795cd125c45f5ad72d68e21a
Dave Antoine <thewizrd.dev@gmail.com>
2024-01-15 16:18:32 -0500

    PlayerWidget: add accent color for enabled state

---
+++ SMTC.GameBar/RepeatButtomTemplateSelector.cs
@@ -0,0 +1,24 @@
+﻿using NPSMLib;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SMTC.GameBar
+{
+    public class RepeatButtonTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            if (item is MediaPlaybackRepeatMode value)
+            {
+                return value switch
+                {
+                    MediaPlaybackRepeatMode.Track => Application.Current.Resources["RepeatTrackTemplate"],
+                    MediaPlaybackRepeatMode.List => Application.Current.Resources["RepeatListTemplate"],
+                    _ => Application.Current.Resources["RepeatNoneTemplate"],
+                } as DataTemplate;
+            }
+
+            return Application.Current.Resources["RepeatNoneTemplate"] as DataTemplate;
+        }
+    }
+}
